3f149990253fe0440268c94dd849dcbe1fa5c3fc
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-04-14 22:21:29 +0530

    Updated Solution - GfG to GitHub

--- Easy/Xoring and Clearing/xoring-and-clearing.java
+++ Easy/Xoring and Clearing/xoring-and-clearing.java
@@ -27,15 +27,22 @@ public class GFG {
 // User function Template for Java
 class Solution {
     public void printArr(int n, int arr[]) {
-        Arrays.stream(arr).forEach(a->System.out.print(a+" "));
+        // code here
+        for(int i : arr)System.out.print(i+" ");
         System.out.println();
     }
 
     public void setToZero(int n, int arr[]) {
-        Arrays.setAll(arr,i->0);
+        // code here
+        for(int i = 0; i < n ; i++){
+            arr[i] = 0;
+        }
     }
 
     public void xor1ToN(int n, int arr[]) {
-        Arrays.setAll(arr,i->arr[i]^i);
+        // code here
+        for(int i = 0; i < n ; i++){
+            arr[i] ^= i;
+        }
     }
 }
