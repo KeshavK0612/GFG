c2b8d701b6d42ee45c9ca152966fa4981ad54948
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-07-07 01:32:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of occurrence/number-of-occurrence.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().count(arr, n, x);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int ans[] = searchRange(arr,x);
+        if (ans[0]==-1) return 0;
+        else{
+            return ans[1] - ans[0] +1;
+        }
+    }
+    
+    public static int[] searchRange(int[] nums, int target) {
+        int[] res = { -1, -1 };
+
+        // Find the first occurrence of target
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        if (start < nums.length && nums[start] == target) {
+            res[0] = start;
+        } else {
+            return res; // Target not found
+        }
+
+        // Find the last occurrence of target
+        start = 0;
+        end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        if (end >= 0 && nums[end] == target) {
+            res[1] = end;
+        }
+
+        return res;
+    }
+}
