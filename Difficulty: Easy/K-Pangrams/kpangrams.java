6f82084262c36574e0f95684ab73cab843e5fe04
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-07-26 22:53:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/K-Pangrams/kpangrams.java
+++ Difficulty: Easy/K-Pangrams/kpangrams.java
@@ -23,25 +23,21 @@ class GFG {
 
 // User function Template for Java
 class Solution {
-    boolean kPangram(String str, int k) {
-        // code here
-        String s="";
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)==' ') continue;
-            else{s+=str.charAt(i);
-            }
-        }
-        if(s.length()<26) return false;
-        HashSet<Character> set=new HashSet<>();
-        for(int i=0;i<s.length();i++){
+    public boolean kPangram(String str, int k) {
+        // Remove spaces and convert to lowercase
+        String s = str.replaceAll(" ", "").toLowerCase();
+
+        // If length is less than 26, it cannot be a pangram
+        if (s.length() < 26) return false;
+
+        // Use a set to count unique characters
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
             set.add(s.charAt(i));
         }
-        if((set.size()+k)>=26){
-            return true;
-        }
-        if(set.size()>=26){
-            return true;
-        }
-        return false;
+
+        // Check if the number of unique characters plus k is at least 26
+        return set.size() + k >= 26;
+        
     }
 }
