42152141641aaf30742178dfb216291f16a6fbc1
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-08-07 00:19:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -1,76 +1,53 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.lang.*;
+import java.util.*;
 
 class GFG {
-	public static void main(String[] args) throws IOException
-	{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine().trim());
-        while(t-->0)
-        {
-            StringTokenizer stt = new StringTokenizer(br.readLine());
-            
-            int n = Integer.parseInt(stt.nextToken());
-            int m = Integer.parseInt(stt.nextToken());
-            int k = Integer.parseInt(stt.nextToken());
-            int a[] = new int[(int)(n)];
-            int b[] = new int[(int)(m)];
-            
-            
-            String inputLine[] = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                a[i] = Integer.parseInt(inputLine[i]);
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine().trim());
+
+            String[] line1 = br.readLine().trim().split(" ");
+            int[] arr1 = new int[line1.length];
+            for (int i = 0; i < line1.length; i++) {
+                arr1[i] = Integer.parseInt(line1[i]);
             }
-            String inputLine1[] = br.readLine().trim().split(" ");
-            for (int i = 0; i < m; i++) {
-                b[i] = Integer.parseInt(inputLine1[i]);
+
+            String[] line2 = br.readLine().trim().split(" ");
+            int[] arr2 = new int[line2.length];
+            for (int i = 0; i < line2.length; i++) {
+                arr2[i] = Integer.parseInt(line2[i]);
             }
-            
-            
-            Solution obj = new Solution();
-            System.out.println(obj.kthElement( a, b, n, m, k));
-            
+
+            Solution ob = new Solution();
+            System.out.println(ob.kthElement(k, arr1, arr2));
         }
-	}
+    }
 }
 
 // } Driver Code Ends
 
 
-//User function Template for Java
-
+// User function Template for Java
 
 class Solution {
-    public long kthElement(int arr1[], int arr2[], int n1, int n2, int k) {
-    // If n1 is bigger, swap the arrays to ensure n1 <= n2
-    if (n1 > n2) {
-        return kthElement(arr2, arr1, n2, n1, k);
-    }
-
-    int low = Math.max(0, k - n2), high = Math.min(k, n1);
-    while (low <= high) {
-        int mid1 = (low + high) / 2;
-        int mid2 = k - mid1;
-        
-        // Calculate l1, l2, r1, and r2
-        int l1 = (mid1 > 0) ? arr1[mid1 - 1] : Integer.MIN_VALUE;
-        int l2 = (mid2 > 0) ? arr2[mid2 - 1] : Integer.MIN_VALUE;
-        int r1 = (mid1 < n1) ? arr1[mid1] : Integer.MAX_VALUE;
-        int r2 = (mid2 < n2) ? arr2[mid2] : Integer.MAX_VALUE;
-
-        if (l1 <= r2 && l2 <= r1) {
-            return Math.max(l1, l2);
-        } else if (l1 > r2) {
-            high = mid1 - 1;
-        } else {
-            low = mid1 + 1;
+    public long kthElement(int k, int arr1[], int arr2[]) {
+        // code here
+        int arrSize=arr1.length+arr2.length;
+        int[] resArr=new int[arrSize];
+        for(int i=0;i<arr1.length;i++){
+            resArr[i]=arr1[i];
         }
+        int j=0;
+        for(int i=arr1.length;i<arrSize;i++,j++){
+            resArr[i]=arr2[j];
+        }
+        Arrays.sort(resArr);
+        return resArr[k-1];
     }
-    return 0; // This line is never reached if inputs are valid
-}
-
 }
