6712df631ab025732760beefc38bd06a68e35ba8
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-08-07 10:35:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -33,21 +33,38 @@ class GFG {
 // } Driver Code Ends
 
 
-// User function Template for Java
+
+
+//User function Template for Java
+
 
 class Solution {
     public long kthElement(int k, int arr1[], int arr2[]) {
-        // code here
-        int arrSize=arr1.length+arr2.length;
-        int[] resArr=new int[arrSize];
-        for(int i=0;i<arr1.length;i++){
-            resArr[i]=arr1[i];
-        }
-        int j=0;
-        for(int i=arr1.length;i<arrSize;i++,j++){
-            resArr[i]=arr2[j];
-        }
-        Arrays.sort(resArr);
-        return resArr[k-1];
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+    if (n1 > n2) {
+        return kthElement(k, arr2, arr1 );
     }
+
+    int low = Math.max(0, k - n2), high = Math.min(k, n1);
+    while (low <= high) {
+        int mid1 = (low + high) / 2;
+        int mid2 = k - mid1;
+        
+        int l1 = (mid1 > 0) ? arr1[mid1 - 1] : Integer.MIN_VALUE;
+        int l2 = (mid2 > 0) ? arr2[mid2 - 1] : Integer.MIN_VALUE;
+        int r1 = (mid1 < n1) ? arr1[mid1] : Integer.MAX_VALUE;
+        int r2 = (mid2 < n2) ? arr2[mid2] : Integer.MAX_VALUE;
+
+        if (l1 <= r2 && l2 <= r1) {
+            return Math.max(l1, l2);
+        } else if (l1 > r2) {
+            high = mid1 - 1;
+        } else {
+            low = mid1 + 1;
+        }
+    }
+    return 0; 
+}
+
 }
