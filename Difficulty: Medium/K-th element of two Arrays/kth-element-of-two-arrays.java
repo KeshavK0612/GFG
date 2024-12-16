eb9922eb166174e8845650da1b267405458452a4
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-12-16 20:06:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -13,19 +13,20 @@ class GFG {
             int k = Integer.parseInt(br.readLine().trim());
 
             String[] line1 = br.readLine().trim().split(" ");
-            int[] arr1 = new int[line1.length];
+            int[] a = new int[line1.length];
             for (int i = 0; i < line1.length; i++) {
-                arr1[i] = Integer.parseInt(line1[i]);
+                a[i] = Integer.parseInt(line1[i]);
             }
 
             String[] line2 = br.readLine().trim().split(" ");
-            int[] arr2 = new int[line2.length];
+            int[] b = new int[line2.length];
             for (int i = 0; i < line2.length; i++) {
-                arr2[i] = Integer.parseInt(line2[i]);
+                b[i] = Integer.parseInt(line2[i]);
             }
 
             Solution ob = new Solution();
-            System.out.println(ob.kthElement(k, arr1, arr2));
+            System.out.println(ob.kthElement(a, b, k));
+            System.out.println("~");
         }
     }
 }
@@ -33,38 +34,39 @@ class GFG {
 // } Driver Code Ends
 
 
-
-
-//User function Template for Java
-
+// User function Template for Java
 
 class Solution {
-    public long kthElement(int k, int arr1[], int arr2[]) {
-        int n1 = arr1.length;
-        int n2 = arr2.length;
-    if (n1 > n2) {
-        return kthElement(k, arr2, arr1 );
-    }
+ public int kthElement(int a[], int b[], int k) {
+    int n1 = a.length;
+    int n2 = b.length;
+    int i = 0, j = 0, count = 0;
 
-    int low = Math.max(0, k - n2), high = Math.min(k, n1);
-    while (low <= high) {
-        int mid1 = (low + high) / 2;
-        int mid2 = k - mid1;
-        
-        int l1 = (mid1 > 0) ? arr1[mid1 - 1] : Integer.MIN_VALUE;
-        int l2 = (mid2 > 0) ? arr2[mid2 - 1] : Integer.MIN_VALUE;
-        int r1 = (mid1 < n1) ? arr1[mid1] : Integer.MAX_VALUE;
-        int r2 = (mid2 < n2) ? arr2[mid2] : Integer.MAX_VALUE;
-
-        if (l1 <= r2 && l2 <= r1) {
-            return Math.max(l1, l2);
-        } else if (l1 > r2) {
-            high = mid1 - 1;
+    while (i < n1 && j < n2) {
+        if (a[i] < b[j]) {
+            count++;
+            if (count == k) return a[i];
+            i++;
         } else {
-            low = mid1 + 1;
+            count++;
+            if (count == k) return b[j];
+            j++;
         }
     }
-    return 0; 
+
+    while (i < n1) {
+        count++;
+        if (count == k) return a[i];
+        i++;
+    }
+
+    while (j < n2) {
+        count++;
+        if (count == k) return b[j];
+        j++;
+    }
+    
+    return -1; 
 }
 
 }
